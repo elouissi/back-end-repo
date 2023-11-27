3655aba7e12df465d3b4d919084a1b9bd81231e5
elouissi <yassinelouissi67@gmail.com>
2023-11-27 19:42:26 +0100

    ajouter deux tableaux category et sous category

--- javascript/freelances.js
+++ javascript/freelances.js
@@ -1,6 +1,7 @@
 
 var btn = document.getElementById("btnmodal")
 var x =document.getElementById("close-modal")
+var btnmodal_two =document.getElementById("btnmodal_two")
 var last =document.getElementById("last_btn_close")
 var list = document.getElementById("default-modal")
  
@@ -18,6 +19,19 @@ var list = document.getElementById("default-modal")
 
  })
  last.addEventListener("click", ()=>{
+
+ 
+    list.classList.toggle("hidden")
+
+ })
+ last.addEventListener("click", ()=>{
+
+ 
+    list.classList.toggle("hidden")
+
+ })
+ btnmodal_two.addEventListener("click", ()=>{
+   alert("hhhh")
 
  
     list.classList.toggle("hidden")
